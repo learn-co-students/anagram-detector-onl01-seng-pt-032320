9ce53f54fd56c3675fc41b41dbb2a029bf37824e
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-17 22:35:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+requre 'pry'
+
+class Anagram
+  
+  attr_accessor :word
+  
+  initialize(word)
+    @word = word
+  end
+  
+  def match
+    array = []
+    binding.pry
+  end
+  
+end
