23349be0e71dc65b009066b9e399f632c94dd934
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-18 20:34:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,20 @@
-requre 'pry'
+require 'pry'
 
 class Anagram
+
+ attr_accessor :word
   
-  attr_accessor :word
-  
-  initialize(word)
+  def initialize(word)
     @word = word
   end
   
-  def match
-    array = []
-    binding.pry
+  def match(list_of_words)
+    output = []
+    list_of_words.each do |word|
+      output << word if word.split("").sort == @word.split("").sort
+    #binding.pry
+    end
+    output
   end
   
 end
