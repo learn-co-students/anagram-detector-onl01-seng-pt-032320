035f38b558918bf044d72f275997811bfeee6773
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-20 21:07:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    match_array = @word.split("").sort
+    new_array = []
+    array.each do |word|
+      word = word.split("")
+      match_array == word.sort ? new_array << word.join("") : false
+      # binding.pry
+    end
+    new_array
+  end
+
+end
