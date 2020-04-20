79e4d91ddaaadec6640f0fa0eb64f989acc0c1ea
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-20 22:42:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+attr_accessor :the_word
+
+def initialize(a_word)
+@the_word = a_word
+end
+
+def match(the_list_of_words_i_guess)
+  the_list_of_words_i_guess.select do |x|
+    @the_word.split("").sort ==  x.split("").sort
+end
+end
+
+end
